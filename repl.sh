8973fd9e89be5dcf77964329db17c7bcc78fc46a
Mateus Junior <mateus@matj.dev>
2024-10-04 12:49:16 +0200

    fix(repl): Correct indentation and remove line-ending semicolons

--- repl.sh
+++ repl.sh
@@ -20,45 +20,48 @@ ps=(
 	PS._RIGHT
 	PS._PS
 	"PS._SELECT|PS._SHARE"
-    "PS._START|PS._OPTIONS"
-    PS._RX
-    PS._RY
-    PS._LX
-    PS._LY
-    GET_L1
-    PRESS_L1
+	"PS._START|PS._OPTIONS"
+	PS._RX
+	PS._RY
+	PS._LX
+	PS._LY
+	GET_L1
+	PRESS_L1
 )
 
 xb=(
-    XB1_A
-    XB1_B
-    XB1_X
-    XB1_Y
-    XB1_LB
-    XB1_RB
-    XB1_LT
-    XB1_RT
-    XB1_LS
-    XB1_RS
-    XB1_UP
-    XB1_DOWN
-    XB1_LEFT
-    XB1_RIGHT
-    XB1_XBOX
-    XB1_VIEW
-    XB1_MENU
-    XB1_RX
-    XB1_RY
-    XB1_LX
-    XB1_LY
-    GET_LB
-    PRESS_LB
+	XB1_A
+	XB1_B
+	XB1_X
+	XB1_Y
+	XB1_LB
+	XB1_RB
+	XB1_LT
+	XB1_RT
+	XB1_LS
+	XB1_RS
+	XB1_UP
+	XB1_DOWN
+	XB1_LEFT
+	XB1_RIGHT
+	XB1_XBOX
+	XB1_VIEW
+	XB1_MENU
+	XB1_RX
+	XB1_RY
+	XB1_LX
+	XB1_LY
+	GET_LB
+	PRESS_LB
 )
 
 num_buttons=${#ps[@]}
 
 for i in $(seq 0 $((num_buttons-1)))
 do
-    echo sed -i -r "s/${ps[$i]}/${xb[$i]}/g" "$target_file"
-    sed -i -r "s/${ps[$i]}/${xb[$i]}/g" "$target_file"
+	echo sed -i -r "s/${ps[$i]}/${xb[$i]}/g" "$target_file"
+	sed -i -r "s/${ps[$i]}/${xb[$i]}/g" "$target_file"
 done
+
+# Remove semi-colons at the end of lines
+sed -i -r 's/;$//g' "$target_file"
